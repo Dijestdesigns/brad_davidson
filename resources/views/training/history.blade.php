bd102e616d4480a9bd05cc1466b12e2e350fec19
JaydeepMor <it.jaydeep.mor@gmail.com>
2020-10-31 17:37:10 +0530

    Update training history.

--- resources/views/training/history.blade.php
+++ resources/views/training/history.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="border-head">
-                    <h3><i class="fa fa-angle-right"></i> {{ __('Training') }}</h3>
+                    <h3><i class="fa fa-angle-right"></i> {{ __('Client training history') }}</h3>
                 </div>
             </div>
         </div>
