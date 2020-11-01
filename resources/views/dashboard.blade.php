b71677fd6a869a2e930836d510f17fe8e00720c8
JaydeepMor <it.jaydeep.mor@gmail.com>
2020-11-02 00:38:21 +0530

    Update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,11 +2,7 @@
 
 @section('content')
     <section class="wrapper">
-        <div class="row">
-            <div class="col-lg-12 text-center">
-                <img src="{{ asset('img/icons/UCC-Logo-dark.png') }}" src="" height="120" width="200">
-            </div>
-        </div>
+        @include('ultimateLogo')
     </section>
 
     <section class="wrapper">
