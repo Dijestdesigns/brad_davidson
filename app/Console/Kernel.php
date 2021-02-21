7e74df8d639383c1e940aa95db4a8962e8df13fe
JaydeepMor <it.jaydeep.mor@gmail.com>
2021-02-21 16:36:23 +0530

    Remove cron test.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('import:moxi:users')->everyMinute();
+        $schedule->command('import:moxi:users')->everyTenMinutes();
     }
 
     /**
